b0b1904f7dff6744118c08a3c54f4fd810ab59c4
songzhipeng <songzhipengmune@126.com>
2016-05-16 11:33:32 +0800

    modified execFile.js spawn function

--- 20160516/execFile.js
+++ 20160516/execFile.js
@@ -1,6 +1,19 @@
 var child_process = require('child_process');
 
 var path = '.';
-child_process.execFile('/bin/ls', ['-l', path], function(err, result){
-  console.log(result);
+// child_process.execFile('/bin/ls', ['-l', path], function(err, result){
+//   console.log(result);
+// });
+
+var ls = child_process.spawn('ls ', ['-l', path]);
+ls.stdout.on('data', function(data){
+  console.log('stdout: ' + data);
+});
+
+ls.stderr.on('data', function(data){
+  console.log('stderr: ' + data);
+});
+
+ls.on('close', function(code){
+  console.log('child process exited with code : ' + code);
 });
