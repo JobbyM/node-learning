5d5c5d45efad3cb9ba48f6195686c81b90e97aad
songzhipeng <songzhipengmune@126.com>
2016-05-18 19:48:58 +0800

    add online and exit events in cluster.js

--- 20160518/cluster.js
+++ 20160518/cluster.js
@@ -3,9 +3,23 @@ var os = require('os');
 var http = require('http')
 
 if(cluster.isMaster){
-  for(var i = 0, n = os.cpus().length; i < n; i += 1){
+  var numWorkers = os.cpus().length;
+  console.log('Master cluster setting up ' + numWorkers + ' workers...');
+  for(var i = 0; i < numWorkers; i += 1){
     cluster.fork();
   }
+
+  cluster.on('online', function(worker){
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+
+  cluster.on('exit', function(workder, code, signal){
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code +
+      ', and signal: ' + signal);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+  
 }else{
   http.createServer(function(req, res){
     res.writeHead(200);
