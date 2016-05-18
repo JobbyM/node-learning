57793834d07bf887260c441e7df1c68326287f47
songzhipeng <songzhipengmune@126.com>
2016-05-18 19:19:31 +0800

    add cluster.js in 20160518

---
+++ 20160518/cluster.js
@@ -0,0 +1,14 @@
+var cluster = require('cluster');
+var os = require('os');
+var http = require('http')
+
+if(cluster.isMaster){
+  for(var i = 0, n = os.cpus().length; i < n; i += 1){
+    cluster.fork();
+  }
+}else{
+  http.createServer(function(req, res){
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8000);
+}
