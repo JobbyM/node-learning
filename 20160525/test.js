ce2e8fbc8597ae0d7faf5d24f2f9b3a428eb8cae
JobbyM <songzhipengmune@gmail.com>
2016-05-27 15:57:19 +0800

    add fs mkdirSync writeFileSync readFileSync

--- 20160525/test.js
+++ 20160525/test.js
@@ -1,1 +1,7 @@
-console.log('test');
+var fs = require('fs');
+
+fs.mkdirSync('./helloDirSync', 0777);
+fs.writeFileSync('./helloDirSync/message.txt', 'Hello Node');
+var data = fs.readFileSync('./helloDirSync/message.txt', 'UTF-8');
+console.log('file created with contents:');
+console.log(data);
