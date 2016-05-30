0eede6a8a541e410b834f87834d52b9e2aae47e8
JobbyM <songzhipengmune@gmail.com>
2016-05-30 10:13:23 +0800

    add readdir in test.js

--- 20160525/test.js
+++ 20160525/test.js
@@ -6,21 +6,39 @@ var fs = require('fs');
 // console.log('file created with contents:');
 // console.log(data);
 
-fs.readdir(process.cwd(), function(err, files){
-  if(err){
-    console.log(err);
-    return;
-  }
+// fs.readdir(process.cwd(), function(err, files){
+//   if(err){
+//     console.log(err);
+//     return;
+//   }
+//
+//   var count = files.length;
+//   var results = {}
+//   files.forEach(function(filename){
+//       fs.readFile(filename, function(data){
+//         results[filename] = data;
+//         count --;
+//         if(count <= 0){
+//           // 对所有文件进行处理
+//         }
+//       })
+//   });
+// });
 
-  var count = files.length;
-  var results = {}
-  files.forEach(function(filename){
-      fs.readFile(filename, function(data){
-        results[filename] = data;
-        count --;
-        if(count <= 0){
-          // 对所有文件进行处理
-        }
-      })
+fs.readdir('/', function(err ,files){
+  if(err) throw err;
+
+  files.forEach(function(file){
+    fs.stat('/' + file, function(err, stats){
+      if(err) throw err;
+
+      if(stats.isFile()){
+        console.log('%s is file', file);
+      }
+      else if(stats.isDirectory){
+        console.log('%s is a directory', file);
+      }
+      console.log('stats: %s', JSON.stringify(stats));
+    });
   });
 });
