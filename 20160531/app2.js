818617bc7712dd2dad96daeb8fa28b7eddfa39c9
JobbyM <songzhipengmune@gmail.com>
2016-05-31 17:51:24 +0800

    add stdin stdout stream

--- 20160531/app2.js
+++ 20160531/app2.js
@@ -10,4 +10,17 @@
 //   .pipe(zlib.createGzip())
 //   .pipe(process.stdout);
 
-process.stdin.pipe(process.stdout)
+// process.stdin.pipe(process.stdout)
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function(){
+  var chunk = process.stdin.read();
+  if(chunk !== null){
+    process.stdout.write('data: ' + chunk);
+  }
+});
+
+process.stdin.on('end', function(){
+  process.stdout.write('end')
+});
