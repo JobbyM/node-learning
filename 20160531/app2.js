320f36b953b37f41faab91f23ce76cf7f2216dcb
JobbyM <songzhipengmune@gmail.com>
2016-05-31 17:41:08 +0800

    add process.stdin

--- 20160531/app2.js
+++ 20160531/app2.js
@@ -3,9 +3,11 @@
 // fs.createReadStream('wow.txt')
 //   .pipe(process.stdout);
 
-var fs = require('fs');
-var zlib = require('zlib');
+// var fs = require('fs');
+// var zlib = require('zlib');
+//
+// fs.createReadStream('wow.txt')
+//   .pipe(zlib.createGzip())
+//   .pipe(process.stdout);
 
-fs.createReadStream('wow.txt')
-  .pipe(zlib.createGzip())
-  .pipe(process.stdout);
+process.stdin.pipe(process.stdout)
