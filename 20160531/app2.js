b187e3dfd84ae08606f727381d6a38d63e03c84b
JobbyM <songzhipengmune@gmail.com>
2016-05-31 17:37:14 +0800

    add pipe process.stdout

--- 20160531/app2.js
+++ 20160531/app2.js
@@ -1,4 +1,11 @@
+// var fs = require('fs');
+//
+// fs.createReadStream('wow.txt')
+//   .pipe(process.stdout);
+
 var fs = require('fs');
+var zlib = require('zlib');
 
 fs.createReadStream('wow.txt')
+  .pipe(zlib.createGzip())
   .pipe(process.stdout);
