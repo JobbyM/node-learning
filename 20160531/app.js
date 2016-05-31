7bd19ad974cc05ffd4f8d588896b6ec2dec25bfd
JobbyM <songzhipengmune@gmail.com>
2016-05-31 16:24:21 +0800

    add process in app.js

---
+++ 20160531/app.js
@@ -0,0 +1,13 @@
+console.log(process.argv);
+console.log('---');
+console.log(process.env);
+console.log('---');
+console.log(process.installPrefix);
+console.log('---');
+console.log(process.pid);
+console.log('---');
+console.log(process.platform);
+console.log('---');
+console.log(process.title);
+console.log('---');
+console.log(process.version);
