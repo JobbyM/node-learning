3f02402665e38fa6b1574bf3e9ba439562dbd4ab
JobbyM <songzhipengmune@gmail.com>
2016-06-01 15:20:25 +0800

    add process.on SIGTERM

--- 20160531/app3.js
+++ 20160531/app3.js
@@ -5,15 +5,29 @@
 //   console.log('SIGINT 信号，按Control-D 退出');
 // })
 
-process.on('uncaughtException', function(err){
-  console.error('An uncaught error occurred!');
-  console.error(err.stack);
-  // throw new Error('未捕获错误')
+// process.on('uncaughtException', function(err){
+//   console.error('An uncaught error occurred!');
+//   console.error(err.stack);
+//   // throw new Error('未捕获错误')
+// });
+//
+// setTimeout(function(){
+//   console.log('本行依然执行');
+// },500)
+//
+// process.on('exit', code =>
+//   console.log('exiting with code: ' + code))
+
+process.on('SIGTERM', function(){
+  console.log('terminating');
+  process.exit(1);
 });
 
 setTimeout(function(){
-  console.log('本行依然执行');
-},500)
+  console.log('sending SIGTERM to process %d', process.pid);
+  process.kill(process.pid, 'SIGTERM');
+}, 500);
 
-process.on('exit', code =>
-  console.log('exiting with code: ' + code))
+setTimeout(function(){
+  console.log('never called');
+}, 1000);
