fecb07bba77b07d86737dfd4d4542e843400758b
JobbyM <songzhipengmune@gmail.com>
2016-06-01 16:23:39 +0800

    add copy.js version 2.0

--- 20160601/copy.js
+++ 20160601/copy.js
@@ -6,18 +6,26 @@ console.log(process.argv[2], '->', process.argv[3]);
 var readStream = fs.createReadStream(process.argv[2]);
 var writeStream = fs.createWriteStream(process.argv[3]);
 
-readStream.on('data', function(chunk){
-  writeStream.write(chunk);
+readStream.on('open', function(){
+  readStream.pipe(writeStream);
 });
 
 readStream.on('end', function(){
   writeStream.end();
 });
 
-readStream.on('error', function(err){
-  console.log('ERROR',err);
-});
-
-writeStream.on('error', function(err){
-  console.log('ERROR',err);
-});
+// readStream.on('data', function(chunk){
+//   writeStream.write(chunk);
+// });
+//
+// readStream.on('end', function(){
+//   writeStream.end();
+// });
+//
+// readStream.on('error', function(err){
+//   console.log('ERROR',err);
+// });
+//
+// writeStream.on('error', function(err){
+//   console.log('ERROR',err);
+// });
