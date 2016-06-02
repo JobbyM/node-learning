0f2f63024d42a7fb5e079033a9e7f2f5ade936f2
JobbyM <songzhipengmune@gmail.com>
2016-06-02 15:24:15 +0800

    add createWriteStream.on pipe

--- 20160601/app3.js
+++ 20160601/app3.js
@@ -1,11 +1,22 @@
+// var fs = require('fs');
+//
+// var writer = fs.createWriteStream('write.txt');
+//
+// for(var i = 0; i < 100; i ++){
+//   writer.write('hello, #' + i + '!\n');
+// }
+// writer.end('this is the end\n');
+// writer.on('finish', function(){
+//   console.error('all writes are now complete.');
+// })
+
 var fs = require('fs');
+var assert = require('assert');
 
 var writer = fs.createWriteStream('write.txt');
-
-for(var i = 0; i < 100; i ++){
-  writer.write('hello, #' + i + '!\n');
-}
-writer.end('this is the end\n');
-writer.on('finish', function(){
-  console.error('all writes are now complete.');
-})
+var reader = fs.createReadStream('wow.txt');
+writer.on('pipe',function(src){
+  console.error('something is piping into the writer');
+  assert.equal(src, reader);
+});
+reader.pipe(writer);
