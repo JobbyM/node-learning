32d325357b3dd35aee9cdf436a219ab5662cde21
JobbyM <songzhipengmune@gmail.com>
2016-06-02 10:53:57 +0800

    add readableStream.pause()

--- 20160601/app.js
+++ 20160601/app.js
@@ -5,9 +5,16 @@ var chunk;
 
 readableStream.setEncoding('utf8');
 
-// readableStream.on('data',function(chunk){
-//   data += chunk;
-// });
+readableStream.on('data',function(chunk){
+  // data += chunk;
+  console.log('读取d%字节的数据', chunk.length);
+  readableStream.pause();
+  console.log('接下来的1秒内不读取数据');
+  setTimeout(function(){
+    console.log('数据恢复读取');
+    readableStream.resume();
+  },1000);
+});
 
 // readableStream.on('readable',function(){
 //   while((chunk=readableStream.read())!==null){
@@ -15,9 +22,9 @@ readableStream.setEncoding('utf8');
 //   }
 // });
 
-readableStream.resume();
+// readableStream.resume();
 
 readableStream.on('end', function(){
-  // console.log(data);
-  console.log('数据流到达尾部，未读取任何任务数据');
+  console.log(data);
+  // console.log('数据流到达尾部，未读取任何任务数据');
 });
