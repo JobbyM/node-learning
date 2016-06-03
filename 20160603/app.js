510cab183bbb799c95a0647d0750026fe0ed30ef
JobbyM <songzhipengmune@gmail.com>
2016-06-03 15:03:44 +0800

    add path.resolve

--- 20160603/app.js
+++ 20160603/app.js
@@ -1,2 +1,10 @@
 var path = require('path');
 console.log(path.join(__dirname,'foo'));
+
+console.log(path.resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile'));
+
+console.log(path.resolve('/foo/bar', './baz'));
+
+console.log(path.resolve('/foo/bar', '/tmp/file/'));
+
+console.log(path.resolve('wwwroot','static_files/png/','../gif/image.gif'));
