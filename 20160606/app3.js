a2eb6fe4fc1899ff87a27ebfaa746be45d7ca824
JobbyM <songzhipengmune@gmail.com>
2016-06-06 11:05:58 +0800

    add assert.throws()

--- 20160606/app3.js
+++ 20160606/app3.js
@@ -1,4 +1,35 @@
 var assert = require('assert');
 
-assert.strictEqual(1, '1', '预期严格相等')
+assert.strictEqual(1, 1, '预期严格相等')
 assert.notStrictEqual(1, true, '预期严格不相等')
+
+// 抛出的错误符合某个构造函数
+assert.throws(
+  function(){
+    throw new Error('Wrong value');
+  },
+  Error,
+  '不符合预期的错误类型'
+)
+
+// 抛出错误的提示信息符合正则表达式
+assert.throws(
+  function(){
+    throw new Error('Wrong value');
+  },
+  /value/,
+  '不符合预期的错误类型'
+)
+
+// 抛出的错误符合自定义函数的校验
+assert.throws(
+  function(){
+    throw new Error('Wrong value');
+  },
+  function(err){
+    if( (err instanceof Error) && /value/.test(err)){
+      return true;
+    }
+  },
+  '不符合预期的错误类型'
+)
