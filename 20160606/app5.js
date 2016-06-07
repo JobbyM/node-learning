53886cc09fbd9d4ad9b5338ccc60af17f2b1932b
JobbyM <songzhipengmune@gmail.com>
2016-06-07 15:29:57 +0800

    add koa-router

--- 20160606/app5.js
+++ 20160606/app5.js
@@ -1,6 +1,15 @@
 var koa = require('koa');
 var app = koa();
 
+var Router = require('koa-router');
+
+var myRouter = new Router();
+
+myRouter.get('/',function* (){
+  this.response.body = 'Hello World!'
+})
+
+app.use(myRouter.routes())
 // app.use(function* (next){
 //   if(this.path === '/'){
 //     this.body = 'we are at home!'
@@ -15,28 +24,28 @@ var app = koa();
 //   this.body = 'we are at home!'
 // })
 
-// normal route
-app.use(function* (next){
-  if(this.path !== '/'){
-    return yield next;
-  }
-  this.body = 'hello world'
-});
-
-// /404 route
-app.use(function* (next){
-  if(this.path !== '/404'){
-    return yield next;
-  }
-  this.body = 'page not found'
-})
-
-// /500 route
-app.use(function* (next){
-  if(this.path !== '/500'){
-    return yield next;
-  }
-  this.body = 'internal server error'
-})
+// // normal route
+// app.use(function* (next){
+//   if(this.path !== '/'){
+//     return yield next;
+//   }
+//   this.body = 'hello world'
+// });
+//
+// // /404 route
+// app.use(function* (next){
+//   if(this.path !== '/404'){
+//     return yield next;
+//   }
+//   this.body = 'page not found'
+// })
+//
+// // /500 route
+// app.use(function* (next){
+//   if(this.path !== '/500'){
+//     return yield next;
+//   }
+//   this.body = 'internal server error'
+// })
 
 app.listen(3000)
