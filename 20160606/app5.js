7d69631c2f5001c3642b4f84ac6a8d85a3226b61
JobbyM <songzhipengmune@gmail.com>
2016-06-07 15:15:55 +0800

    add koa multi path router

--- 20160606/app5.js
+++ 20160606/app5.js
@@ -9,10 +9,34 @@ var app = koa();
 //   }
 // })
 
-// 等同于
+// // 等同于
+// app.use(function* (next){
+//   if(this.path !== '/') return yield next;
+//   this.body = 'we are at home!'
+// })
+
+// normal route
 app.use(function* (next){
-  if(this.path !== '/') return yield next;
-  this.body = 'we are at home!'
+  if(this.path !== '/'){
+    return yield next;
+  }
+  this.body = 'hello world'
+});
+
+// /404 route
+app.use(function* (next){
+  if(this.path !== '/404'){
+    return yield next;
+  }
+  this.body = 'page not found'
+})
+
+// /500 route
+app.use(function* (next){
+  if(this.path !== '/500'){
+    return yield next;
+  }
+  this.body = 'internal server error'
 })
 
 app.listen(3000)
